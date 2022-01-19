011c70cbcbfcaa7b1faa7215480685d86dc7958b
Alieze-Ali <71855821+Alieze-Ali@users.noreply.github.com>
2022-01-18 20:12:39 -0800

    server fully functioning

--- api/server.js
+++ api/server.js
@@ -1,9 +1,21 @@
 const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const helmet = require('helmet');
 
 const server = express();
 
+//middlewares
+server.use(helmet())
+server.use(morgan('dev'))
+server.use(cors())
+
 server.get('/', (req, res) => {
-    res.send('I love node!')
+    res.status(200).json({
+        status: 200,
+        message: 'this server is live baby',
+        time: new Date().toLocaleTimeString(),
+    })
 })
 
 module.exports = server
